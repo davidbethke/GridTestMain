3a67d277cc40749f32d262dff5f6025fd8de6f9a
Dave Bethke <david.bethke@gmail.com>
2012-03-21 22:29:58 -0500

    check strings

--- GridTestMain.cpp
+++ GridTestMain.cpp
@@ -20,6 +20,19 @@ int main()
 	people[0][0]=Person("dave",18);
 	cout << people;
 	getchar();
+	GridT<string> myStrings;
+	myStrings[0][0]="String1";
+	cout<< myStrings;
+
+	for(int i=0; i<10;++i)
+		for(int j=0;j<10;++j)
+			if(i==j)
+				myStrings[i][j]="X";
+			else
+				myStrings[i][j]="O";
+
+	cout << myStrings;
+	getchar();
 	return 0;
 }
 
